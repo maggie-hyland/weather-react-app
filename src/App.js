0b7c4297741994ff16022e1a5717e9d8f6053f83
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2022-10-16 11:12:30 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
+
 function App() {
   let city = "Lisbon";
 
--- src/Weather.js
+++ src/Weather.js
@@ -11,5 +11,9 @@ export default function Weather(props) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+  return (
+    <div>
+      <h2>Hello from Weather</h2>{" "}
+    </div>
+  );
 }
