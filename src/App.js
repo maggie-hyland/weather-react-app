5fcf399a430df109712fa3dc0b8b96d4419c38e3
maggie-hyland <96745029+maggie-hyland@users.noreply.github.com>
2022-10-16 20:30:20 -0400

    imported from CodeSandbox & moved items around

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
 import React from "react";
+import Bootstrap from "bootstrap";
 import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>This is my Weather App</h1>
-        <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
-          SheCodes
-        </a>
-        <img src="test.png" alt="SheCodes Logo" />
-        <Weather city="Tokyo" />
-      </header>
+      <div class="container" id="weather-app-container">
+        <div class="card" id="container-card">
+          <div class="card-body">
+            <div class="container text-center">
+              <div class="row">
+                <div class="col-5" id="app-name">
+                  <i class="fa-regular fa-hand-peace"></i> Get Ready Forecast
+                </div>
+              </div>
+              <div id="bottom-container">
+                <div class="row">
+                  <div class="col-6">
+                    <div>
+                      <Weather />
+                    </div>
+                  </div>
+                  <div class="col-6" id="icon-current-temp-c-f">
+                    <span id="temp-icon">
+                      <img src="" alt="" id="icon" class="temp-icon" />
+                    </span>
+                    <span id="temperature"></span>
+                    <span class="units">
+                      <a href="#" id="celsius-link" class="active">
+                        °C
+                      </a>{" "}
+                      |
+                      <a href="#" id="fahrenheit-link">
+                        °F
+                      </a>
+                    </span>
+                  </div>
+                </div>
+                <div class="row"></div>
+                <div id="weather-forecast"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col" id="open-source">
+          Developed by Maggie Hyland. Open-sourced on
+          <a
+            href="https://github.com/maggie-hyland/vanilla-weather-app"
+            id="github-link"
+          >
+            GitHub
+          </a>
+          and hosted on Netlify.
+        </div>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import "./Weather.css";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}°C.`
+export default function Weather() {
+  let [temp, setTemp] = useState(null);
+  let [city, setCity] = useState(null);
+  let [humidity, setHumid] = useState(null);
+  let [wind, setWind] = useState(null);
+  let [description, setDescript] = useState(null);
+  let [icon, setIcon] = useState(null);
+
+  function showTemp(response) {
+    console.log(response.data);
+    setTemp(response.data.main.temp);
+    setHumid(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setDescript(response.data.weather.description);
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
   }
-  let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(handleResponse);
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemp);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div>
-      <h2>Hello from Weather</h2>{" "}
+      <form onSubmit={handleSubmit}>
+        <input type="search" onChange={updateCity} />
+        <input type="submit" value="submit" />
+      </form>
+      <ul class="weather-list">
+        <li>
+          <div id="city">City: {city}</div>
+        </li>
+        <li>
+          {" "}
+          <span>
+            Last updated: <span id="date"></span>
+          </span>
+        </li>
+        <li>Temperature: {Math.round(temp)}°C</li>
+        <li>
+          {" "}
+          <div id="description" class="weather-details">
+            Description: {description}
+          </div>
+        </li>
+        <li>
+          {" "}
+          <div class="weather-details">Humidity: {Math.round(humidity)}%</div>
+        </li>
+        <li>
+          {" "}
+          <div class="weather-details">Wind: {Math.round(wind)}km/h</div>
+        </li>
+        <li>
+          <img src={icon} alt=" " />
+        </li>
+      </ul>
     </div>
   );
 }
